f7dba607127d168cf47c9560dd3a583cbad5afad
Ando Avetisyan <andoavetis0127@gmail.com>
2022-06-24 14:47:48 +0400

    Added a footer for todo list

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -1,7 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
 //styles
 import "../styles/TodoInput.scss";
+//components
+import TodoBox from "./TodoBox";
+import Completeds from "./Completeds";
 
 export default function TodoInput() {
-  return <div className="todo-main">TodoInput</div>;
+  const [todo, setTodo] = useState("");
+  const [todoList, setTodoList] = useState([]);
+
+  const addToDo = (todos) => {
+    if (todos !== "") {
+      const newTodo = {
+        id: Math.random(),
+        text: todos,
+        completed: false,
+      };
+      setTodoList([...todoList, newTodo]);
+    } else {
+      alert("Wrong text");
+    }
+    setTodo("");
+  };
+
+  const removeTodo = (id) => {
+    setTodoList([...todoList.filter((todo) => todo.id !== id)]);
+  };
+
+  const onChange = (newTodo) => {
+    return setTodoList(
+      todoList.map((elem) => {
+        if (elem.id === newTodo.id) {
+          return newTodo;
+        }
+        return elem;
+      })
+    );
+  };
+  return (
+    <div className="main">
+      <div className="main_div">
+        <input
+          onChange={(e) => setTodo(e.target.value)}
+          className="main_div_input"
+          type="text"
+          placeholder="What you have to do?"
+          value={todo}
+        />
+        <button onClick={() => addToDo(todo)} className="main_div_btn">
+          Add
+        </button>
+      </div>
+      <div className="main_todos">
+        <div className="main_todos_comp">
+          {todoList.map((todo) => {
+            return (
+              <TodoBox
+                key={todo.id}
+                todo={todo}
+                removeTodo={removeTodo}
+                onChange={onChange}
+              />
+            );
+          })}
+          <Completeds todoList={todoList} />
+        </div>
+      </div>
+    </div>
+  );
 }
